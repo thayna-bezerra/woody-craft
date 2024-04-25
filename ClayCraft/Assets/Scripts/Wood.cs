4950cd5858923a6b0ea9bf0d125d7ae9479829d6
thayna-bezerra <arraisthayna2106@gmail.com>
2024-04-25 15:43:38 -0300

    woot rotation

---
+++ ClayCraft/Assets/Scripts/Wood.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine ;
+using DG.Tweening ;
+
+public class Wood : MonoBehaviour {
+   [SerializeField] private SkinnedMeshRenderer skinnedMeshRenderer ;
+   [SerializeField] private Transform woodTransform ;
+   [SerializeField] private Vector3 rotationVector ;
+   [SerializeField] private float rotationDuration ;
+
+   private void Start () {
+      woodTransform
+         .DOLocalRotate (rotationVector, rotationDuration, RotateMode.FastBeyond360)
+         .SetLoops (-1, LoopType.Restart)
+         .SetEase (Ease.Linear) ;
+   }
+}
