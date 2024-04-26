69000a9590db705b2f965f11be623985bf14414a
thayna-bezerra <arraisthayna2106@gmail.com>
2024-04-26 17:06:55 -0300

    colliders

--- ClayCraft/Assets/Scripts/Knife.cs
+++ ClayCraft/Assets/Scripts/Knife.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class Knife : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
+    [SerializeField] private float hitDamage;
+    [SerializeField] private Wood wood;
+
+    [SerializeField] private ParticleSystem woodFx;
+
+    private ParticleSystem.EmissionModule woodFxEmission;
 
     private Rigidbody knifeRb;
     private Vector3 movementVector;
@@ -11,6 +17,8 @@ public class Knife : MonoBehaviour
     private void Start()
     {
         knifeRb = GetComponent<Rigidbody>();
+
+        woodFxEmission = woodFx.emission;
     }
 
     private void Update()
@@ -25,5 +33,24 @@ public class Knife : MonoBehaviour
     {
         if (isMoving)
             knifeRb.position += movementVector;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        woodFxEmission.enabled = false;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Coll coll = collision.collider.GetComponent<Coll>();
+        if (coll != null)
+        {
+            // hit Collider:
+            woodFxEmission.enabled = true;
+            woodFx.transform.position = collision.contacts[0].point;
+
+            coll.HitCollider(hitDamage);
+            wood.Hit(coll.index, hitDamage);
+        }
     }
 }
--- ClayCraft/Assets/Scripts/Wood.cs
+++ ClayCraft/Assets/Scripts/Wood.cs
@@ -1,16 +1,25 @@
-﻿using UnityEngine ;
-using DG.Tweening ;
+﻿using UnityEngine;
+using DG.Tweening;
 
-public class Wood : MonoBehaviour {
-   [SerializeField] private SkinnedMeshRenderer skinnedMeshRenderer ;
-   [SerializeField] private Transform woodTransform ;
-   [SerializeField] private Vector3 rotationVector ;
-   [SerializeField] private float rotationDuration ;
+public class Wood : MonoBehaviour
+{
+    [SerializeField] private SkinnedMeshRenderer skinnedMeshRenderer;
+    [SerializeField] private Transform woodTransform;
+    [SerializeField] private Vector3 rotationVector;
+    [SerializeField] private float rotationDuration;
 
-   private void Start () {
-      woodTransform
-         .DOLocalRotate (rotationVector, rotationDuration, RotateMode.FastBeyond360)
-         .SetLoops (-1, LoopType.Restart)
-         .SetEase (Ease.Linear) ;
-   }
+    private void Start()
+    {
+        woodTransform
+           .DOLocalRotate(rotationVector, rotationDuration, RotateMode.FastBeyond360)
+           .SetLoops(-1, LoopType.Restart)
+           .SetEase(Ease.Linear);
+    }
+
+    public void Hit(int keyIndex, float damage)
+    {
+        float colliderHeight = 2.39705f;
+        float newWeight = skinnedMeshRenderer.GetBlendShapeWeight(keyIndex) + damage * (100f / colliderHeight);
+        skinnedMeshRenderer.SetBlendShapeWeight(keyIndex, newWeight);
+    }
 }
