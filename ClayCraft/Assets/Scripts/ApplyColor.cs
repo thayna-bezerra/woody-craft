6f3bb5b8b0b747bb4eac91c16ea050d6b614046f
thayna-bezerra <arraisthayna2106@gmail.com>
2024-05-03 23:40:38 -0300

    change color of the wood using color picker

---
+++ ClayCraft/Assets/Scripts/ApplyColor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyColor : MonoBehaviour
+{
+    public FlexibleColorPicker fcp;
+    public Material material;
+
+    private void Update()
+    {
+        material.color = fcp.color;
+    }
+}
