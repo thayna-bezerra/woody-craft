d543d80d4ee837051b2b45f456b12485a1e3c973
thayna-bezerra <arraisthayna2106@gmail.com>
2024-05-04 14:45:04 -0300

    change color particle according to color wood material

--- ClayCraft/Assets/Scripts/ApplyColor.cs
+++ ClayCraft/Assets/Scripts/ApplyColor.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ApplyColor : MonoBehaviour
 {
     public FlexibleColorPicker fcp;
+    public ParticleSystem particle;
     public Material material;
+
 
     private void Update()
     {
         material.color = fcp.color;
+                                   
+        Color colorMax = fcp.color;
+
+        Color colorMin = new Color(colorMax.r, colorMax.g, colorMax.b, 0f);
+
+        ParticleSystem.MinMaxGradient gradient = new ParticleSystem.MinMaxGradient(colorMin, colorMax);
+
+        var mainModule = particle.main;
+        mainModule.startColor = gradient;
     }
 }
   
