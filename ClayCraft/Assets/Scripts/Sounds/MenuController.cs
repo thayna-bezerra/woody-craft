8d27988703123d5969d27ffd09962f6d36c65dee
thayna-bezerra <arraisthayna2106@gmail.com>
2024-05-18 18:24:49 -0300

    refactoring controller scripts

--- ClayCraft/Assets/Scripts/Sounds/MenuController.cs
+++ ClayCraft/Assets/Scripts/Sounds/MenuController.cs
@@ -1,29 +1,72 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-    public Button soundButton;
-    public Sprite soundOnSprite;
-    public Sprite soundOffSprite;
+    public GameObject panelHome;
+    public GameObject panelCustomColor;
 
+    public GameObject woodyAnimation;
+    public GameObject woodpeckerAnimation;
+    public GameObject woodModel;
+
+    public Button soundButton;
+    public Sprite soundOn;
+    public Sprite soundOff;
+
+    public AudioClip buttonClickSound;
     private bool isSoundOn;
 
     void Start()
     {
-        // Inicializa o estado do som com o estado armazenado no AudioManager
         isSoundOn = AudioManager.isSoundOn;
         UpdateButtonImage();
 
-        // Adiciona um listener para o botão para chamar a função ToggleSound quando clicado
         soundButton.onClick.AddListener(ToggleSound);
+    }
+
+    public void GoToCustomColor()
+    {
+        PlaySoundEffect(buttonClickSound);
+
+        panelHome.SetActive(false);
+        panelCustomColor.SetActive(true);
+        woodyAnimation.SetActive(false);
+        woodpeckerAnimation.SetActive(false);
+        woodModel.SetActive(true);
+    }
+
+    public void ChangeScene(string name)
+    {
+        PlaySoundEffect(buttonClickSound);
+        SceneManager.LoadScene(name);
+    }
+
+    public void GoHome()
+    {
+        PlaySoundEffect(buttonClickSound);
+
+        panelCustomColor.SetActive(false);
+        panelHome.SetActive(true);
+        woodyAnimation.SetActive(true);
+        woodpeckerAnimation.SetActive(true);
+        woodModel.SetActive(false);
+    }
+
+    public void PlaySoundEffect(AudioClip sound)
+    {
+        AudioManager audioManager = FindObjectOfType<AudioManager>();
+        if (audioManager != null)
+        {
+            audioManager.PlaySoundEffect(sound);
+        }
     }
 
     void ToggleSound()
     {
         isSoundOn = !isSoundOn;
 
-        // Encontra o AudioManager e chama a função ToggleSound
         AudioManager audioManager = FindObjectOfType<AudioManager>();
         if (audioManager != null)
         {
@@ -37,11 +80,11 @@ public class MenuController : MonoBehaviour
     {
         if (isSoundOn)
         {
-            soundButton.image.sprite = soundOnSprite;
+            soundButton.image.sprite = soundOn;
         }
         else
         {
-            soundButton.image.sprite = soundOffSprite;
+            soundButton.image.sprite = soundOff;
         }
     }
 }
--- ClayCraft/Assets/Scripts/System/GameController.cs
+++ ClayCraft/Assets/Scripts/System/GameController.cs
@@ -3,52 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject panelHome;
-    public GameObject panelCustomColor;
-
-    public GameObject woodyAnimation;
-    public GameObject woodpeckerAnimation;
-    public GameObject woodModel;
-
     public AudioClip buttonClickSound;
+    public AudioClip woodpeckerKnocking;
 
-    public void ReloadScene()
-    {
-        PlaySoundEffect(buttonClickSound);
-
-        string currentSceneName = SceneManager.GetActiveScene().name;
-        SceneManager.LoadScene(currentSceneName);
-    }
-
-    public void GoToCustomColor()
-    {
-        PlaySoundEffect(buttonClickSound);
-
-        panelHome.SetActive(false);
-        panelCustomColor.SetActive(true);
-        woodyAnimation.SetActive(false);
-        woodpeckerAnimation.SetActive(false);
-        woodModel.SetActive(true);
-    }
-
-    public void GoHome()
-    {
-        PlaySoundEffect(buttonClickSound);
-
-        panelCustomColor.SetActive(false);
-        panelHome.SetActive(true);
-        woodyAnimation.SetActive(true);
-        woodpeckerAnimation.SetActive(true);
-        woodModel.SetActive(false);
-    }
-
-    public void BackInitialScreen(string name)
-    {
-        PlaySoundEffect(buttonClickSound);
-        SceneManager.LoadScene(name);
-    }
-
-    public void StartGame(string name)
+    public void ChangeScene(string name)
     {
         PlaySoundEffect(buttonClickSound);
         SceneManager.LoadScene(name);
@@ -62,5 +20,4 @@ public class GameController : MonoBehaviour
             audioManager.PlaySoundEffect(sound);
         }
     }
-
 }
